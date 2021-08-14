59668092a284d20d4af1219854832b00a187e83b
Franco Urra <kofran.cl@gmail.com>
2021-08-14 13:16:39 -0400

    version 4
    Se agrega funcion cambiar color al banner del menu de navegacion.
    el cambio es aleatorio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
-window.alert("Bienvenidos a mi Sitio");
+
 
 
 var d = new Date();
 document.getElementById("fecha").innerHTML = d;
+
+
+
+
+function dame_numero_aleatorio(superior, inferior){
+    var numPosibilidades = (superior + 1) - inferior;
+    var aleat = Math.random() * numPosibilidades;
+    aleat = Math.floor(aleat);
+    aleat = (inferior + aleat);
+    return aleat
+ }
+
+function dame_color_aleatorio(){
+    var color_aleat="#"
+    hexadecimal = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+    var inferior = 0;
+    var superior = hexadecimal.length-1;
+    for (i=0;i<6;i++){
+       color_aleat += hexadecimal[dame_numero_aleatorio(superior, inferior)];
+    }
+    return color_aleat
+ }
+
+ 
+
+ 
+
+  function cambiarcolor(){
+    let  Changecolor = document.getElementById('Changecolor')
+
+    Changecolor.style.backgroundColor = dame_color_aleatorio();
+  }
