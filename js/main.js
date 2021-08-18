e9e19ceb91e08fa2e101ea1b769860c5641e982b
Franco Urra <kofran.cl@gmail.com>
2021-08-17 21:51:36 -0400

    version 5
    se agrega el plugin jquery scrollup en el footer.

--- js/main.js
+++ js/main.js
@@ -35,3 +35,19 @@ function dame_color_aleatorio(){
 
     Changecolor.style.backgroundColor = dame_color_aleatorio();
   }
+
+
+ 
+ 
+  $(function () {
+   $.scrollUp({
+     scrollName: 'scrollUp', // Element ID
+     topDistance: '100', // Distance from top before showing element (px)
+     topSpeed: 300, // Speed back to top (ms)
+     animation: 'fade', // Fade, slide, none
+     animationInSpeed: 200, // Animation in speed (ms)
+     animationOutSpeed: 200, // Animation out speed (ms)
+     scrollText: 'Scroll to top', // Text for element
+     activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+   });
+ });
